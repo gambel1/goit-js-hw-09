e61d20eb535dd27e4ee21fec82f3aae15689d1d3
Алексей <electrifyx@gmail.com>
2023-02-22 14:11:22 +0200

    add task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,44 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', createHandlePromise);
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function createHandlePromise(event) {
+  event.preventDefault();
+  const formData = new FormData(event.currentTarget);
+  const dataParams = {};
+
+  for (const [key, value] of formData.entries()) {
+    dataParams[key] = Number(value);
   }
+
+  let { delay, step, amount } = dataParams;
+
+  for (let i = 0; i < amount; i += 1) {
+    delay += step;
+    createPromise(i, delay).then(onSuccess).catch(onError);
+    form.reset();
+  }
+}
+
+function onSuccess({ position, delay }) {
+  Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+}
+
+function onError({ position, delay }) {
+  Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
 }
